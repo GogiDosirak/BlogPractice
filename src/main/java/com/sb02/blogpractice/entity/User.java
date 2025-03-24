215559096fbc549b3c01bf9223c2ad13b7387049
GogiDosirak <154963143+GogiDosirak@users.noreply.github.com>
2025-03-24 16:02:20 +0900

    Feat: User 엔티티 추가

---
+++ src/main/java/com/sb02/blogpractice/entity/User.java
@@ -0,0 +1,19 @@
+package com.sb02.blogpractice.entity;
+
+import java.time.Instant;
+
+public class User {
+    private final String id;
+    private String password;
+    private String email;
+    private String nickname;
+    private Instant createdAt;
+
+    public User(String id, String password, String email, String nickname, Instant createdAt) {
+        this.id = id;
+        this.password = password;
+        this.email = email;
+        this.nickname = nickname;
+        this.createdAt = Instant.now();
+    }
+}
